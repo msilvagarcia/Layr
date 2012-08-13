f2fd3aa1ee99ed6ae6795248cfd0f03e3c9513ca
Miere Teixeira <miere00@gmail.com>
2012-08-13 20:12:47 -0300

    Added missing file

---
+++ layr-core/src/main/java/layr/util/InvalidTypeSerializer.java
@@ -0,0 +1,17 @@
+package layr.util;
+
+import java.lang.reflect.Type;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+public class InvalidTypeSerializer implements JsonSerializer<Object> {
+
+	@Override
+	public JsonElement serialize(Object src, Type typeOfSrc,
+			JsonSerializationContext context) {
+		return null;
+	}
+
+}

f1aebaf2fd82e621ce45fb2835ce3f8ba3154c1c
Miere Teixeira <miere00@gmail.com>
2012-11-20 10:13:15 -0300

    #10

--- layr-engine/src/main/java/org/layr/engine/components/template/Foreach.java
+++ layr-engine/src/main/java/org/layr/engine/components/template/Foreach.java
@@ -14,21 +14,29 @@ public class Foreach extends GenericComponent {
 
 	@Override
 	public void render() throws IOException {
-		String string = getAttribute("var").toString();
-		Object collection = getParsedAttribute("value");
+		String definedVar = getAttribute("var").toString();
+		Object collection = getParsedAttribute("values");
 
 		Collection<?> values = (Collection<?>)collection;
-		if (values != null){
-			int i = 0;
-			for (Object value : values) {
-				requestContext.put(string, value);
-				requestContext.put(string+":i", i++);
-				for (IComponent child : getChildren()) {
-					child.setRequestContext(requestContext);
-					child.render();
-				}
-			}
-		}
+		if (values != null)
+			iterateAndRenderChildrenNodes(definedVar, values);
+	}
 
+	public void iterateAndRenderChildrenNodes(String definedVar, Collection<?> values)
+			throws IOException {
+		int iterationCount = 0;
+		for (Object value : values)
+			iterationCount = renderChildNode(definedVar, iterationCount, value);
+	}
+
+	public int renderChildNode(String definedVar, int i, Object value)
+			throws IOException {
+		requestContext.put(definedVar, value);
+		requestContext.put(definedVar+":i", i++);
+		for (IComponent child : getChildren()) {
+			child.setRequestContext(requestContext);
+			child.render();
+		}
+		return i;
 	}
 }

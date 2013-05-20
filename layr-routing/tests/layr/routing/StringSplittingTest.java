c9fea60b08df3a748f88cb4c83fea0d14ed30920
Miere Teixeira <miere00@gmail.com>
2013-05-19 21:41:50 -0300

    Fixes #65

--- layr-routing/tests/layr/routing/StringSplittingTest.java
+++ layr-routing/tests/layr/routing/StringSplittingTest.java
@@ -43,7 +43,7 @@ public class StringSplittingTest {
 		assertEquals( "blah=123", splitted[1]);
 	}
 	
-	@Test( timeout = 100 )
+	@Test( timeout = 150 )
 	public void grantThatSplitManyTimes(){
 		for ( int i=0; i<MANY_TIMES; i++ )
 			grantThatSplitterWorksAsExpected();

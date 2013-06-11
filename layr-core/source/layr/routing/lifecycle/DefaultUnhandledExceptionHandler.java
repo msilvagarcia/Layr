ed881a1be6f2e9a6286b33892e179395d49fa0f4
Miere Teixeira <miere00@gmail.com>
2013-06-11 19:25:33 -0300

    Closes #75

--- layr-core/source/layr/routing/lifecycle/DefaultUnhandledExceptionHandler.java
+++ layr-core/source/layr/routing/lifecycle/DefaultUnhandledExceptionHandler.java
@@ -1,5 +1,10 @@
 package layr.routing.lifecycle;
 
+import static layr.api.ResponseBuilder.template;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import layr.api.ExceptionHandler;
 import layr.api.RequestContext;
 import layr.api.Response;
@@ -8,8 +13,18 @@ public class DefaultUnhandledExceptionHandler implements ExceptionHandler<Throwa
 
 	@Override
 	public Response render(Throwable exception, RequestContext requestContext) {
-		System.out.println("Rendered oh yeah!");
-		return null;
+		List<StackTraceElement> list = extractStackTraceAsList(exception);
+		return template("layr/routing/lifecycle/DefaultUnhandledExceptionTemplate.xhtml")
+				.set("stack", list)
+				.set("errorMessage", exception.getMessage());
+	}
+
+	private List<StackTraceElement> extractStackTraceAsList(Throwable exception) {
+		ArrayList<StackTraceElement> list = new ArrayList<StackTraceElement>();
+		
+		for ( StackTraceElement stackTraceElement : exception.getStackTrace() )
+			list.add(stackTraceElement);
+		return list;
 	}
 
 }
